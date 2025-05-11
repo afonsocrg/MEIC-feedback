9d79e08eb6be23dd04a0cede8be1bc495bd87801
afonsocrg <afonso.crg@gmail.com>
2025-05-11 21:57:40 +0700

    added email, schoolYear,workloadRating and approvedAt columns to feedback table

---
+++ backend/src/db/migrations/0005_add_more_data_to_feedback.sql
@@ -0,0 +1,4 @@
+ALTER TABLE `feedback` ADD `email` text;--> statement-breakpoint
+ALTER TABLE `feedback` ADD `school_year` integer;--> statement-breakpoint
+ALTER TABLE `feedback` ADD `workload_rating` integer;--> statement-breakpoint
+ALTER TABLE `feedback` ADD `approved_at` integer;
--- backend/src/db/schema/feedback.ts
+++ backend/src/db/schema/feedback.ts
@@ -3,17 +3,24 @@ import { courses } from './course'
 
 export const feedback = sqliteTable('feedback', {
   id: integer('id').primaryKey({ autoIncrement: true }),
+  email: text('email'),
+  schoolYear: integer('school_year'),
   courseId: integer('course_id')
     .notNull()
     .references(() => courses.id),
   rating: integer('rating').notNull(),
+  workloadRating: integer('workload_rating'),
   comment: text('comment'),
+  originalComment: text('original_comment'),
+
+  // This is the date the feedback was approved
+  // If null, it means the feedback is pending approval,
+  // and should not be shown in the public-facing pages
+  approvedAt: integer('approved_at', { mode: 'timestamp' }),
+
   createdAt: integer('created_at', { mode: 'timestamp' }).$defaultFn(
     () => new Date()
-  ),
-
-  // This column is just used internally to store the original comment, in case we need to review something
-  originalComment: text('original_comment')
+  )
 })
 
 export type Feedback = typeof feedback.$inferSelect
